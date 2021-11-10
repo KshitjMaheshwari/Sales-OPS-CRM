6a4526ad7984c290ccacead7f923b6d4156a73d1
kshitij.maheshwari_cs19 <kshitij.maheshwari_cs19@gla.ac.in>
2021-11-10 15:49:57 +0530

    update login functionality

---
+++ Login/LoginPage.js
@@ -0,0 +1,8 @@
+function check(form) {
+
+    if (form.userid.value == "salesops@gmail.com" && form.pswrd.value == "123456") {
+        window.open('../index.html', "_self")
+    } else {
+        alert("Error Password or Email Address")
+    }
+}
